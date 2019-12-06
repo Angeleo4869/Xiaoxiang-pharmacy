70c70042ca41585eeecdfd65015f636af2b70423
xiaofeng <2653458427@qq.com>
2019-12-06 15:45:16 +0800

    basic Inteface

---
+++ Code/react-app/src/Test/useful.jsx
@@ -0,0 +1,21 @@
+
+{/*
+外链跳转打开新页面
+<span>
+    <a href="https://www.baidu.com" 
+        target="_blank"
+        rel="noopener noreferrer">
+        管理员入口
+    </a>
+</span>
+//样式并且悬浮样式
+
+sstyle:{
+
+},
+'&:hover': {
+    color: '#40a9ff',
+    opacity: 1,
+  },
+
+*/ }
